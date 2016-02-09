a533ac9a88dd058bfc62a8c94573ac21630e8af4
Infodinamica <contacto@infodinamica.cl>
2016-02-09 18:58:49 -0300

    Primera versión
    Se crea la primera versión del proyecto. Permite exportar listas
    genericas a XLS y XLSX

---
+++ src/Infodinamica.Framework.Exportable/Infodinamica.Framework.Exportable/Engines/Excel/IExcelExportEngine.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Infodinamica.Framework.Exportable.Engines.Excel
+{
+    public interface IExcelExportEngine: IExportEngine
+    {
+        void AddData<T>(IList<T> data, string sheetName) where T : class;
+        void SetFormat(ExcelVersion version);
+    }
+}
